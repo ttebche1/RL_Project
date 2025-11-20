be2e11fdfae09eb7c016b7f36776f5d396eada90
Tanya Tebcherani <ttebche1@jh.edu>
2025-11-19 20:45:59 -0500

    Updating main

--- train_SAC.py
+++ train_SAC.py
@@ -35,6 +35,9 @@ if __name__ == "__main__":
     # Record start time
     start_time = time.perf_counter()
 
+    # Set values
+    is_auv = True  # Whether the agent is an AUV (True) or ASV (False)
+
     # User parameters
     num_envs = 16               # Number of parallel environments
     batch_size = 512            # Number of samples used from the buffer per gradient update
@@ -43,26 +46,31 @@ if __name__ == "__main__":
     tau = 0.01                  # Target network update rate (slow updates)
     gamma = 0.99                # Discount factor for future rewards (heavily considers future rewards)
     train_freq = 4              # How often to update the NNs
-    gradient_steps = 1          # How many gradient steps to take during each update
+    gradient_steps = 4          # How many gradient steps to take during each update
     learning_rate = 3e-4        # How fast the NNs update
-    total_timesteps = int(3e6)  # Total timesteps to train the agent
+    if is_auv:
+        total_timesteps = int(3e6)  # Total timesteps to train the agent
+    else:
+        total_timesteps = int(1e6)
     env_params = {
         "env_size": 1000.0,             # Distance from the origin in all four directions in meters
         "target_radius": 300.0,         # Radius for "found" condition in meters
         "max_steps_per_episode": 200,   # Max steps per episode
-        "max_velocity": 1.0,            # Agent velocity in m/s
         "turn_rate": 2,                 # Agent turning rate in deg/s
         "max_current": 0.5,             # Max current in m/s 
         "dt": 30,                       # Time step in seconds
         "dist_noise_std": 1.0,          # Standard deviation of Gaussian noise added to distance measurements in meters
         "yaw_noise_std": 0.1,           # Standard deviation of Gaussian noise added to yaw actions in radians
-        "render_mode": None,             # No rendering during training
-        "is_auv": True,                 # Whether the agent is an AUV (True) or ASV (False)
+        "render_mode": None,            # No rendering during training
+        "is_auv": is_auv,               # Whether the agent is an AUV (True) or ASV (False)
         "rho": 1025,                    # Seawater density in kg/m^3          
         "hotel_power": 30,              # Hotel load power in Watts
         "drag_coeff": 0.0079,           # Drag coefficient
         "area": 12.507,                 # Area in m^2
-        "eta": 0.5                      # Propulsion efficiency
+        "eta": 0.5,                     # Propulsion efficiency
+        "const_vel": False,             # Whether to use constant (True) or variable (False) velocity
+        "max_velocity": 1.0,            # Agent max velocity in m/s if const_vel is False
+        "velocity": 1.0                 # Agent constant velocity in m/s if const_vel is True
     }
 
     # Create vectorized environments with training result logs
